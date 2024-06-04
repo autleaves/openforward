1c70d1d928da99e1151600a045b55d8f03a6d8a1
4-1@201 <4-1@201.school>
2024-06-04 11:57:39 +0900

    r6-6-4 201

---
+++ inside_web/php/0528/check.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>PHP基礎</title>
+</head>
+<body>
+
+<?php
+$nickname=$_POST['nickname'];
+$email = $_POST['email'];
+$goiken = $_POST['goiken'];
+
+if($nickname==''){
+    print'にゅうりょくしてね<br>';
+}
+else{
+print'ようこそ';
+print $nickname;
+print'様';    
+print'<br>'; 
+}
+if($email==''){
+    print'メールアドレスが入力されていません。<br>';
+}
+else{
+print'ようこそ';
+print $email;
+print'<br>';    
+}
+if($goiken==''){
+    print'ご意見が入力されていません。<br>';
+}
+else{
+print'ご意見「';
+print $goiken;
+print'」<br>'; 
+}
+?>
+
+</body>
+</html>
